e0b8ee1146835bfa2e125679208080597852c408
roshan <roshanlipu100@gmail.com>
2023-10-07 17:54:41 +0530

    updated app.js to index.js  for deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 const MongoStore = require("connect-mongo");
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port =  process.env.PORT || 5000;
 
 app.use(
   session({
